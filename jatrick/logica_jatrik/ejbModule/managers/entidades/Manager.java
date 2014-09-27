55da107868553cf3ee2c4f7e4bd998b5bb2aa9c7
plateaolimpica <plateaolimpica@gmail.com>
2014-09-27 20:55:09 -0300

    *

---
+++ jatrick/logica_jatrik/ejbModule/managers/entidades/Manager.java
@@ -0,0 +1,30 @@
+package managers.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotNull;
+
+import equipos.entidades.Equipo;
+
+@Entity
+public class Manager {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Column(unique = true)
+	private String username;
+	
+	@NotNull
+	private String password;
+	
+	@OneToOne
+	private Equipo equipo;
+
+}
