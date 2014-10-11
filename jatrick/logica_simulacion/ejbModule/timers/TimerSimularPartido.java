e0b3305b131e40ecd924c419f83fd74c65259bb7
matycarro <matycarro@gmail.com>
2014-10-11 20:11:43 -0200

    va

--- jatrick/logica_simulacion/ejbModule/timers/TimerSimularPartido.java
+++ jatrick/logica_simulacion/ejbModule/timers/TimerSimularPartido.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import excepciones.NoExisteEquipoExcepcion;
 import partido.LogicaSimulacion;
 import partido.PartidoControlador;
 import partidos.Partido;
@@ -24,14 +25,21 @@ public class TimerSimularPartido {
 	@Inject
 	LogicaSimulacion lsim;
 	
+	@Inject
+	PartidoControlador cp;
+	
 	public void crearTimerSimularPartido(int idPartido,Calendar c){
 		ts.createTimer(c.getTime(), idPartido);
 	}
 	
 	@Timeout
-	public void simularPartido(Timer t){
-		int idPartido = (int)t.getInfo();
-		Partido p = new Partido();
+	public void simularPartido(Timer t) throws NoExisteEquipoExcepcion{
+		long idPartido = (long)t.getInfo();
+		Partido p = cp.find(idPartido);
+		if(p==null)
+		{
+			throw new NoExisteEquipoExcepcion("No existe equipo de id " + idPartido);
+		}
 		lsim.simular(p);
 	}
 
