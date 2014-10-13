e95a3d100a12028ceff049afa35228a151d72c9f
plateaolimpica <plateaolimpica@gmail.com>
2014-10-13 19:37:14 -0200

    * + funciones

--- jatrick/logica_simulacion/ejbModule/partido/PartidoControlador.java
+++ jatrick/logica_simulacion/ejbModule/partido/PartidoControlador.java
@@ -2,7 +2,6 @@ package partido;
 
 
 import java.util.Calendar;
-import java.util.LinkedList;
 import java.util.List;
 
 import javax.ejb.LocalBean;
@@ -10,8 +9,8 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.apache.commons.lang.NotImplementedException;
-
+import partidos.Comentario;
+import partidos.ConfiguracionPartido;
 import partidos.Partido;
 
 
@@ -19,7 +18,7 @@ import partidos.Partido;
 @LocalBean
 public class PartidoControlador {
 
-	@PersistenceContext( unitName = "mongoUnit" ) 
+	@PersistenceContext( unitName = "jatrik" ) 
 	private EntityManager em;
 	
 	public PartidoControlador(){
@@ -39,53 +38,22 @@ public class PartidoControlador {
     
     
     
-    public void crear(){
+    public void crearPartido(){
     	Partido p = new Partido();
     	em.persist(p);
     }
+    
+    public void crearComentario(String mensaje, Partido partido, int minuto){
+    	Comentario c = new Comentario();
+    	c.setMensaje(mensaje);
+    	c.setPartido(partido);
+    	c.setMinuto(minuto);
+    	em.persist(c);
+    }
 	
-//	public User login(String usr, String pass){
-//		List<User> found = em.createNamedQuery("User.login", User.class)
-//		.setParameter("usr", usr)
-//		.setParameter("pass", pass)
-//		.getResultList();
-//		return found.isEmpty() ? null : found.get(0);
-//	}
-//	
-//	@Produces
-//    @Named("users")
-//    @RequestScoped
-//    public List<User> list() {
-//        return em.createNamedQuery("User.list", User.class).getResultList();
-//    }
-//	
-//	public boolean register(String usr, String pass, String name, String email){
-//		User u = new User();
-//		u.setUsername(usr);
-//		u.setPassword(pass);
-//		u.setName(name);
-//		u.setEmail(email);
-//		
-//		List<Role> roles = new ArrayList<Role>();
-//		roles.add(Role.PLAYER);
-//		
-//		em.persist(u);
-//		
-//		return true;
-//	}
-//	
-//	public Long create(User user) {
-//        em.persist(user);
-//        return user.getId();
-//    }
-//
-//    public void update(User user) {
-//        em.merge(user);
-//    }
-//
-//    public void delete(User user) {
-//        em.remove(em.contains(user) ? user : em.merge(user));
-//    }
+    public ConfiguracionPartido findConfiguracionPartido(){
+    	 return (ConfiguracionPartido)em.createQuery("select c from ConfiguracionPartido c").getSingleResult();
+    }
 
 }
 
