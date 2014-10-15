23043aab619afedbfa2990c392bb45f7234e925f
plateaolimpica <plateaolimpica@gmail.com>
2014-10-14 22:18:59 -0200

    cambios

--- jatrick/logica_jatrik/ejbModule/partidos/PartidoControlador.java
+++ jatrick/logica_jatrik/ejbModule/partidos/PartidoControlador.java
@@ -1,9 +1,13 @@
 package partidos;
 
+import equipos.Equipo;
+import fabricas.HomeFactory;
+import java.util.List;
 import interfaces.IPartidoControlador;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -13,10 +17,16 @@ public class PartidoControlador implements IPartidoControlador {
 	
 	@PersistenceContext( unitName = "jatrik" ) 
 	private EntityManager em;
-
+	
+	@Inject
+	private HomeFactory hf;
+	
 	@Override
-	public int crearPartido() {
-		// TODO Auto-generated method stub
+	public int crearPartidoAmistoso(int codEquipoLocal, int  codEquipoVisitante, Calendar fecha) {
+		Equipo el = hf.getEquipoControlador().findEquipo(codEquipoLocal);
+		Equipo ev = hf.getEquipoControlador().findEquipo(codEquipoVisitante);
+		Partido p = new Partido(el,ev,fecha);
+		em.persist(p);
 		return 0;
 	}
 
