1e32176633e57a64c94d04c3847d0d6dede9be04
matycarro <matycarro@gmail.com>
2014-10-14 23:11:15 -0200

    va

--- jatrick/logica_jatrik/ejbModule/interfaces/IPartidoControlador.java
+++ jatrick/logica_jatrik/ejbModule/interfaces/IPartidoControlador.java
@@ -1,16 +1,18 @@
 package interfaces;
 
 import java.util.List;
+import java.util.Calendar;
 
 import javax.ejb.Remote;
 import datatypes.DatosComentario;
 import excepciones.NoExistePartidoExepcion;
 import partidos.Partido;
 
+
 @Remote
 public interface IPartidoControlador {
 	
-	public int crearPartido();
+	public int crearPartidoAmistoso(int codEquipoLocal, int  codEquipoVisitante, Calendar fecha);
 	
 	public Partido findPartido(int codPartido);
 	
