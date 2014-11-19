d972a1107ec6dde0703a69457dc72c03a79cc17b
juan <juanlema@gmail.com>
2014-11-19 18:54:23 -0200

    aaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ jatrick/android_jatrik/src/com/tsi206/jatrik/LoginActivity.java
@@ -0,0 +1,121 @@
+package com.tsi206.jatrik;
+
+import java.util.concurrent.ExecutionException;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.tsi206.jatrik.util.PrefUtils;
+
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+public class LoginActivity extends ActionBarActivity {
+
+	private String apiServer;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_ACTION_BAR);
+		Handler h = new Handler();
+	    h.post(new Runnable() {     
+	        @SuppressLint("NewApi")
+			@Override
+	        public void run() {
+	            getActionBar().hide();
+	        }
+	    });
+		
+		setContentView(R.layout.activity_login);
+		
+		String loggedInUserName = PrefUtils.getFromPrefs(LoginActivity.this, PrefUtils.PREFS_LOGIN_USERNAME_KEY, null);
+		String loggedInUserPassword = PrefUtils.getFromPrefs(LoginActivity.this, PrefUtils.PREFS_LOGIN_PASSWORD_KEY, null);
+		
+		MyApp app = ((MyApp)getApplicationContext());
+		apiServer = app.getApiServer();
+		
+		if (loggedInUserName != null & loggedInUserPassword != null){
+			this.login(loggedInUserName, loggedInUserPassword);
+			System.out.println("login automático");
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void login(String username, String password){
+String urlString = apiServer + "login/" + username + '/' + password;
+	    
+	    if(username != null && !username.isEmpty() && password != null && !password.isEmpty()) {
+	    	String opcion = "Login";
+	    	try {
+				String result = new RestAPI(new ProgressDialog(this)).execute(opcion, urlString).get();
+				System.out.println(result);
+				
+				if(result.equals("LoginFAIL")){
+					Toast.makeText(this, "Usuario o contraseña incorrectos",
+				    Toast.LENGTH_LONG).show();
+				}else{
+					MyApp app = ((MyApp)getApplicationContext());
+					app.setUsername(username);
+					app.setPassword(password);
+					
+					PrefUtils.saveToPrefs(LoginActivity.this, PrefUtils.PREFS_LOGIN_USERNAME_KEY, username);
+					PrefUtils.saveToPrefs(LoginActivity.this, PrefUtils.PREFS_LOGIN_PASSWORD_KEY, password);
+					
+					//Voy a la segunda pantalla
+					Intent intent = new Intent(this, PrincialActivity.class);
+					startActivityForResult(intent, 0);
+					finish();
+				}
+	    	} catch (RuntimeException e){
+	    		e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
+	}
+	
+	
+	public void loginView(View view){
+		
+		EditText usernameEditText = (EditText) findViewById(R.id.usernameField);
+	    String username = usernameEditText.getText().toString();
+	    
+	    EditText passwordEditText = (EditText) findViewById(R.id.passwordField);
+	    String password = passwordEditText.getText().toString();
+	    
+	    this.login(username, password);
+	}
+}
